609590948244ef99174d7019fc631a8e610b8bcd
liuranchao <kevinliu@qeeka.com>
2015-11-10 11:29:04 +0800

    - 新增AIDL使用

---
+++ app/src/main/java/com/liuranchao/testdemo/activity/aidl/AIDLActivity.java
@@ -0,0 +1,10 @@
+package com.liuranchao.testdemo.activity.aidl;
+
+/**
+ * Description: TODO
+ *
+ * @author liuranchao
+ * @date 15/11/10 上午9:29
+ */
+public class AIDLActivity {
+}
---
+++ app/src/main/java/com/liuranchao/testdemo/service/BookService.java
@@ -0,0 +1,10 @@
+package com.liuranchao.testdemo.service;
+
+/**
+ * Description: TODO
+ *
+ * @author liuranchao
+ * @date 15/11/10 上午10:00
+ */
+public class BookService {
+}
